4faf14c00a30b944ce0a9f5bab3a43158e8ff3b6
Arvat-collab <131161570+Arvat-collab@users.noreply.github.com>
2026-06-28 11:52:19 +0300

    Update and rename Snakerertest.java to Snaketest.java

--- Snaketest.java
+++ Snaketest.java
@@ -2,7 +2,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class Snakerertest {
+public class Snaketest {
     @Test
     public void snakecollision() {
             Snakerer game = new Snakerer(500,500);

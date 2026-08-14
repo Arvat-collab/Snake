40912b22b317903e44bef8cac7a1e624939f1c95
Arvat-collab <131161570+Arvat-collab@users.noreply.github.com>
2026-08-14 22:46:01 +0300

    Refactor Tile class collision detection and equality
    Updated the collides method to handle null input and improved equals and hashCode methods.

--- Tile.java
+++ Tile.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import java.util.Objects;
+
+/**
+ * Reprezintă o celulă (sau o poziție pe grila 2D) din jocul Snake.
+ */
 public class Tile {
-     private int x;
+
+    private int x;
     private int y;
 
     public Tile(int x, int y) {
@@ -24,9 +30,29 @@ public class Tile {
     public void setY(int y) {
         this.y = y;
     }
-public boolean collides(Tile other) {
-    return x == other.getX() &&
-           y == other.getY();
-}
-}
 
+
+    public boolean collides(Tile other) {
+        if (other == null) {
+            return false;
+        }
+        return this.x == other.x && this.y == other.y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Tile tile = (Tile) obj;
+        return x == tile.x && y == tile.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
